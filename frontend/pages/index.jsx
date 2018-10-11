c562ce86f04fbcee76b414c2456c3c6f0c25a6eb
Samuel Gustafsson <samuel.gustafsson@pdb.se>
2018-10-11 22:21:37 +0200

    Move all nav items to navigation component

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import Link from "next/link";
 
 const Home = () => (
   <div>
     <h1>Home</h1>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
   </div>
 );
 
--- frontend/pages/sell.jsx
+++ frontend/pages/sell.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import Link from "next/link";
 
 const Sell = () => (
   <div>
     <h1>Sell</h1>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
   </div>
 );
 
