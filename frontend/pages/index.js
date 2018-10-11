81380a779ed735db9fabbaa6a9687ff9e0a18bd3
Samuel Gustafsson <samuel.gustafsson@pdb.se>
2018-10-11 21:58:21 +0200

    Add page with navigation

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+  </div>
+);
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const Sell = () => (
+  <div>
+    <h1>Sell</h1>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+);
+
+export default Sell;
