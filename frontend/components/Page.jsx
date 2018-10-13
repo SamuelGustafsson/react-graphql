725c026fa1509a5fe1c6ea92f8514eba06a8974e
Samuel Gustafsson <samuel.gustafsson@pdb.se>
2018-10-13 11:16:38 +0200

    Add global styling with styled components

--- frontend/components/Page.jsx
+++ frontend/components/Page.jsx
@@ -3,6 +3,31 @@ import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import Header from "./Header";
 import Meta from "./Meta";
 
+injectGlobal`
+
+/* import custom font */
+@font-face {
+  font-family: 'radnika_next';
+  src: url("/static/radnikanext-medium-webfont.woff2") format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+
+html {
+  box-sizing: border-box;
+  font-size: 10px;
+}
+*,*:before, *:after {
+  box-sizing:inherit;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  font-family: "radnika_next";
+}
+`;
+
 const theme = {
   red: "#FF0000",
   black: "#393939",
