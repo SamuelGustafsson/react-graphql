efee4c414c0b8da8d5b003e81ecfe90f30080ba9
Samuel Gustafsson <samuel.gustafsson@pdb.se>
2018-10-13 11:15:10 +0200

    Add styling theme to project with styled components

--- frontend/components/Page.jsx
+++ frontend/components/Page.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import Header from "./Header";
 import Meta from "./Meta";
 
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightGrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  boxShadow: "0 12px 24px 0 rgba(0,0,0,0.09)"
+};
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
+      <ThemeProvider theme={theme}>
+          <Meta />
+          <Header />
         <p>Im the page component</p>
-        {this.props.children}
-      </div>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
