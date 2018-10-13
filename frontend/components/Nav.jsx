ca5c4cabc0d840b28aa9e7e194c0c6fd51e8b54c
Samuel Gustafsson <samuel.gustafsson@pdb.se>
2018-10-13 12:41:35 +0200

    Remove home nav item.

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -5,11 +5,20 @@ import NavStyled from "./styles/NavStyles";
 const Nav = () => {
   return (
     <NavStyled>
-      <Link href="/">
-        <a>Home</a>
+      <Link href="/items">
+        <a>items</a>
       </Link>
       <Link href="/sell">
-        <a>Sell</a>
+        <a>sell</a>
+      </Link>
+      <Link href="/signup">
+        <a>sign up</a>
+      </Link>
+      <Link href="/orders">
+        <a>orders</a>
+      </Link>
+      <Link href="/me">
+        <a>account</a>
       </Link>
     </NavStyled>
   );
