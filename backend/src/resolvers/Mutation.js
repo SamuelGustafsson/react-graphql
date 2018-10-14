84f730edd4ea8fb8454898ae64ccb4280979a407
Samuel Gustafsson <samuel.gustafsson@pdb.se>
2018-10-14 21:56:57 +0200

    Add createItem mutation.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, context, info) {
+    // TODO: Check if they are signed
+
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = Mutations;
