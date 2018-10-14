0e96c9e8c0f116314d92a7ad23158a05b007fccf
Samuel Gustafsson <samuel.gustafsson@pdb.se>
2018-10-14 21:58:00 +0200

    Add query to get items
    If there is no special logic to the query, you can use the forwardTo function to let prisma handle the request.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,5 +1,14 @@
 // Here is the place where database calls / data fetching i going to be regardless of what database or data source
 
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db")
+
+  //   async items(parent, AbortSignal, context, info) {
+  //     const items = await context.db.query.items();
+  //     return items;
+  //   }
+};
 
 module.exports = Query;
